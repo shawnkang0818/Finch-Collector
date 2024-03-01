2be93dd7cc80b4d41e419c14cf97e9572aa3de76
Shawn Kang <shawnkang0818@gmail.com>
2024-03-01 02:10:43 -0500

    add details page for finch, add url template tag

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
+from .models import Finch
 # Create views
 def home(request):
-  return HttpResponse('<h1>Hello</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def finch_index(request):
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', { 'finches': finches })
+
+def finch_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch})
