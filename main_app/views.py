bc3bddaee83bb5b6472a0dccd861b40f7ad5eb26
Shawn Kang <shawnkang0818@gmail.com>
2024-03-02 00:30:17 -0500

    add date picker

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 # Create views
 def home(request):
@@ -15,7 +16,8 @@ def finch_index(request):
 
 def finch_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finch})
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form})
 
 class FinchCreate(CreateView):
   model = Finch
